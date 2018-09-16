6f08b44a058679c4dd8073d393366544e435f3fb
church001 <simonreuben23@gmail.com>
2018-09-16 03:18:18 +0100

    added all of the fixes

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,7 +32,7 @@ class BurgerBuilder extends Component {
 
     componentDidMount () {
         console.log(this.props);
-        axios.get( 'https://my-react-001.firebaseio.com//ingredients.json' )
+        axios.get( 'https://my-react-001.firebaseio.com/ingredients.json')
             .then( response => {
                 this.setState( { ingredients: response.data } );
             } )
